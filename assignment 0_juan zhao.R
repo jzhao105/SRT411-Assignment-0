ba13b4879a7331eccd094c88cbc23616075e5de7
jzhao105 <jessicazhao79@gmail.com>
2017-02-03 12:31:26 -0500

    Assignment0 from JUAN ZHAO

---
+++ assignment 0_juan zhao.R
@@ -0,0 +1,44 @@
+##3.1 TODO
+(2017-2014)/(2014-1985)*100
+##3.2 TODO
+Cur_Year = 2017
+My_Birth = 1985
+(Cur_Year -2014)/(2014 - My_Birth)*100
+## 3.3 TODO
+## 3.4 TODO
+a=c(4,5,8,11)
+sum(x=a)
+## 3.5 
+x = rnorm(100)
+plot(x)
+## 4
+help(sqrt)
+## 5 instored in "firstscript.R"
+##  6
+P = seq(31,60)
+Q = matrix(data=P, ncol=5,nrow=6)
+## 6.3
+x1 =rnorm(100)
+x2 = rnorm(100)
+x3 = rnorm(100)
+t = data.frame(a=x1,b=x1+x2,c=x1+x2+x3 )
+t
+plot(t)
+sd(t)
+## 7
+plot(rnorm(100),type="l",col="gold")
+hist(rnorm(100))
+help???????
+## 8 
+D8= read.table(file = "tst1.txt", header =TRUE)
+D8
+
+j=c(1,2,NA)
+max(j)
+max(j,na.rm = TRUE)
+
+
+
+
+
+
